cc4c582541edc579fa3f7d552fa03c72c6e36abf
RenardDeFeu <david.borg@le-campus-numerique.fr>
2022-11-07 16:44:14 +0100

    petit ajout de cs tailwind pour le style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,31 +3,35 @@
     <img class="inline" alt="Vue logo" src="../assets/logo.png">
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
     Number of articles in your cart : {{ userCart.length }}
-    <div class="grid grid-cols-3 gap-4">
-      <div :class="[cartButton && divID == index? 'inline border border-blue-600 p-2' : 'inline border border-black p-2']" v-bind:key="product.id" 
-            v-for="(product, index) in productList" v-on:click="addCartButton(index)">
+    <div class="grid grid-cols-4 gap-4 mx-10">
+      <div :class="[cartButton && divID == index? 'inline border border-blue-600 rounded-lg shadow-lg mb-auto' : 'inline border p-2 rounded-lg shadow-md mb-36']" v-bind:key="product.id" 
+            v-for="(product, index) in productList" @mouseover="addCartButton(index)" @mouseleave="cartButton = false"> <!--v-on:click="addCartButton(index)" mb-36-->
         {{ product.title }} 
         <br>
         <p> <img class="max-h-32 max-w-[25%] inline" v-bind:src="product.image"> </p>
-        $ {{ product.price }} 
-        <br>
-        <button class="rounded-lg bg-blue-600 text-white ml-2 p-2 hover:bg-red-500" v-on:click="addToCart(product)" v-if="cartButton && divID == index">Add To Cart</button>
-        <button class="rounded-lg bg-blue-600 text-white ml-2 p-2 hover:bg-red-500" v-on:click="removeToCart(product)" v-if="cartButton && divID == index">Remove from Cart</button>
+        $ {{ product.price }}
+        <div v-if="cartButton && divID == index">
+          <ProductDetails/> <button class="rounded-lg bg-transparent text-black ml-2 p-2 hover:bg-red-100" v-on:click="null">See the details</button>
+          <br>
+          <button class="rounded-lg bg-blue-600 text-white ml-2 p-2 hover:bg-red-500" v-on:click="addToCart(product)">Add To Cart</button>
+          <button class="rounded-lg bg-blue-600 text-white ml-2 p-2 hover:bg-red-500" v-on:click="removeToCart(product)">Remove from Cart</button>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
+import ProductDetails from '@/components/ProductDetails.vue'
 import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
     //HelloWorld
+    ProductDetails
   },
   data()
   {
@@ -37,7 +41,6 @@ export default {
       divID: null,
 
       userCart: [],
-      //userCartProductTemplate: {article:null, quantity:null}
     }
   },
   methods:{
